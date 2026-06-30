65b3ef721560ec5911e18b9f28b6e19c1a007f02
Jacob Huffaker <jh3881@princeton.edu>
2026-06-29 17:23:52 -0700

    cleaned up slider text language

--- questions.js
+++ questions.js
@@ -2,36 +2,36 @@ const questionText = {
   strength: {
     prompt: "When considering the strength of my cocktail right now, ...",
     low: "I do not want to taste the alcohol at all",
-    high: "I want my drink to taste VERY strong"
+    high: "I want my drink to taste very strong"
   },
 
   sweetness: {
     prompt: "When considering the sweetness of my cocktail right now, ...",
     low: "I strongly dislike sweet flavors",
-    high: "I prefer my drink to be as sweet as possible"
+    high: "I prefer my drink to taste very sweet"
   },
 
   sourness: {
     prompt: "When considering the sourness of my cocktail right now, ...",
     low: "I strongly dislike sour flavors",
-    high: "I prefer very my drink to be as sour as possible"
+    high: "I prefer my drink to taste very sour"
   },
 
   bitterness: {
     prompt: "When considering the bitterness of my cocktail right now, ...",
     low: "I strongly dislike bitter flavors",
-    high: "I prefer my drink to be as bitter as possible"
+    high: "I prefer my drink to taste very bitter"
   },
 
   thickness: {
     prompt: "When considering the texture of my cocktail right now, ...",
-    low: "I want something very thin like water or sprite",
-    high: "I want something very velvety like a smoothie or a coffee with heavy foam"
+    low: "I want something thin like water or sprite",
+    high: "I want something velvety like a smoothie or coffee with heavy foam"
   },
 
   rarity: {
     prompt: "When considering how unusual my cocktail is right now, ...",
-    low: "I want very simple ingredients with expected flavor mixtures",
-    high: "I want something very rare with unique flavor mixtures"
+    low: "I want simple ingredients with expected flavor mixtures",
+    high: "I want something rare with unique flavor mixtures"
   }
 };
