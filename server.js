44d2f37c4c433ba1088a3182006c7dc9f9a2574f
Jacob Huffaker <jh3881@princeton.edu>
2026-08-17 15:12:25 -0700

    sitemap

--- server.js
+++ server.js
@@ -32,7 +32,9 @@ const MIME_TYPES = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
-  ".csv": "text/csv; charset=utf-8"
+  ".csv": "text/csv; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8"
 };
 
 const ROUTE_ALIASES = new Map([
